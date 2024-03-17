70f020ea57b3e84788fc38be0524043710ad364e
hojinYang <hojin.yang7@gmail.com>
2024-03-17 11:46:56 -0400

    added photos

---
+++ api/index.py
@@ -0,0 +1,17 @@
+import os
+import random
+from flask import Flask, send_file
+
+app = Flask(__name__)
+IMAGE_DIR = "images"
+
+@app.route('/')
+def home():
+    return 'Hello, World!'
+
+@app.route('/sample')
+def sample():
+    images = os.listdir(IMAGE_DIR)
+    image_files = [img for img in images]
+    selected_image = random.choice(image_files)
+    return send_file(os.path.join(IMAGE_DIR, selected_image), mimetype='image/jpg')
